19112610d40753b58b426263afdd8aef0df5f7bc
Dobrenikie <123772484+Dobrenikie@users.noreply.github.com>
2023-10-24 19:35:31 +0300

    update

--- main.py
+++ main.py
@@ -9,5 +9,17 @@
 # **
 # *
 
-def triangle():
-    pass
+def print_star_triangle(n):
+    if n % 2 == 0:
+        print("Ошибка! Число должно быть нечетным.")
+        return
+
+    for i in range(1, n):
+        line = "*" * i
+        print(line)
+    for i in range(-n, 1 ):
+        line = "*" * -i
+        print(line)
+
+n = int(input("Введите нечетное натуральное число: "))
+print_star_triangle(n)
